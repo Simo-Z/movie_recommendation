76ab73f9ababb03c130f8b089141cb717a7a727c
Simo-Z <mail@simozilling.de>
2022-05-10 17:19:08 +0200

    Small app changes

--- recommenders/user_based.py
+++ recommenders/user_based.py
@@ -7,5 +7,5 @@ from sklearn.metrics.pairwise import cosine_similarity
 def top_n_movies_user_based(user_id, movies: DataFrame, ratings: DataFrame, n_movies: int = 5):
     
     sparse_matrix = ratings.pivot(index='userId', columns='movieId', values='rating')
-    movies_df = cosine_similarity(sparse_matrix)
+    #cosine_similarity(sparse_matrix)
     return sparse_matrix
